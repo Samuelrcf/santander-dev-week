6fcc3a5afbb0f7580bf3cb874a526c49e7895add
Samuel Rógenes <rogenescarvalho@hotmail.com>
2024-08-17 13:56:53 -0300

    create domain layer

---
+++ src/main/java/com/samuelr/santander_dev_week/domain/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.samuelr.santander_dev_week.domain.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.samuelr.santander_dev_week.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+}
